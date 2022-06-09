57741f03c8ffdb410c69eedaf0c543ebd6327067
Irene <irenemanningm@gmail.com>
2022-06-09 14:26:26 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
+require "pry"
 #write your code here
 
-def countdown
+def countdown(i=10)
+    while i > 0 
+        
+        sleep 1 and puts "#{i} SECOND(S)!"
+        i = i - 1
+    end
+    sleep 1 and puts "HAPPY NEW YEAR!"
 end
+
+# binding.pry
+
+countdown(i)
